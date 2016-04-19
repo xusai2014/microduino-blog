cd73cc66f005ae19e231c392b89cd5d0e9344edc
xusai2014 <13048699388@163.com>
2016-04-19 12:36:54 +0800

    [microduino-jerry]remove the subscribtions callback

--- client/blogroutes.js
+++ client/blogroutes.js
@@ -9,20 +9,7 @@ Router.map(function() {
   this.route('blogIndex', {
     controller: ShopController,
     template: 'blogIndex',
-    path: '/blog',
-    subscriptions: function(){
-      Meteor.subscribe("blogAccounts");
-      var blogAccounts = Meteor.users.find({ "username": "microduinoblog" }).fetch()[0];  
-      blogAccounts && typeof blogAccounts._id != "undefined" && BlogPages.set({
-        sort: {
-          title: -1
-        },
-        filters: {
-          authorId: blogAccounts._id,
-          // type: "project"
-        }
-      });
-    }
+    path: '/blog'
 
   });
 
